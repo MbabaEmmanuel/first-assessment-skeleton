4568a47d3563c265c6a795aa8a7fbc1bc1307927
Emmanuel Mbaba <emmanuel.mbaba32@gmail.com>
2016-08-25 08:03:40 -0500

    v3

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -3,16 +3,11 @@ import { words } from 'lodash'
 import { connect } from 'net'
 import { Message } from './Message'
 
-
 export const cli = vorpal()
 
 let username
 let server
 let lastCommand
-
-
-
-
 
 cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
@@ -20,11 +15,13 @@ cli
 cli
   .mode('connect <username> [ipaddress]')
  .delimiter(cli.chalk['green']('connected>'))
+
   .init(function (args, callback)
+
    {
       username = args.username
-
       server = connect({ host: args.ipaddress, port: 8080 }, () =>
+
       {
         server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       })
@@ -35,6 +32,7 @@ cli
 
       server.on('end', () => {
         cli.exec('exit')
+
     })
       callback()
   })
@@ -42,58 +40,57 @@ cli
     let [ command, ...rest ] = words(input,/[^\s]+/g)
     let contents = rest.join(' ')
 
-    if (command === 'disconnect') {
+    if (command === 'disconnect')
+    {
       server.end(new Message({ username, command }).toJSON() + '\n')
     }
-      else if (command === 'echo')
-        {
+    else if (command === 'echo')
+    {
 
-          server.write(new Message({ username, command, contents }).toJSON() + '\n')
-          lastCommand = 'echo'
-        }
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
+      lastCommand = 'echo'
+    }
 
-      else if (command === 'users')
-      {
-          server.write(new Message({ username, command, contents }).toJSON() + '\n')
+    else if (command === 'users')
+    {
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
 
+    }
+    else if (command === 'broadcast')
+    {
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
+      lastCommand = 'broadcast'
 
-      }
-      else if (command === 'broadcast')
-      {
-          server.write(new Message({ username, command, contents }).toJSON() + '\n')
-          lastCommand = 'broadcast'
-
-      }
-      else if (command.charAt(0) === '@')
-      {
-        server.write(new Message({username, command: command, contents }).toJSON() + '\n')
-        lastCommand = command
-
-      }
-      else if (command !== 'connect' && command !== 'disconnect' && command !== 'echo' && command !== 'broadcast'
+    }
+    else if (command.charAt(0) === '@')
+    {
+      server.write(new Message({username, command: command, contents }).toJSON() + '\n')
+      lastCommand = command
+    }
+    else if (command !== 'connect' && command !== 'disconnect' && command !== 'echo' && command !== 'broadcast'
           && command.charAt(0) !==  '@' && command !== 'users' && lastCommand === 'echo')
-      {
+    {
 
-       server.write(new Message({ username, command: lastCommand, contents: command + ' ' + contents}).toJSON() + '\n')
+      server.write(new Message({ username, command: lastCommand, contents: command + ' ' + contents}).toJSON() + '\n')
 
-     }
-      else if (command !== 'connect' && command !== 'disconnect' && command !== 'echo' && command !== 'broadcast'
+    }
+    else if (command !== 'connect' && command !== 'disconnect' && command !== 'echo' && command !== 'broadcast'
           && command.charAt(0) !==  '@' && command !== 'users' && lastCommand === 'broadcast')
-      {
-        server.write(new Message({ username, command: lastCommand, contents: command + ' ' + contents }).toJSON() + '\n')
+    {
+      server.write(new Message({ username, command: lastCommand, contents: command + ' ' + contents }).toJSON() + '\n')
 
-      }
-      else if (command !== 'connect' && command !== 'disconnect' && command !== 'echo' && command !== 'broadcast'
+    }
+    else if (command !== 'connect' && command !== 'disconnect' && command !== 'echo' && command !== 'broadcast'
           && command.charAt(0) !==  '@' && command !== 'users' && lastCommand.charAt(0) === '@')
-      {
+    {
         server.write(new Message({ username, command: lastCommand, contents: command + ' ' + contents }).toJSON() + '\n')
 
-      }
+    }
 
-      else
-      {
+    else
+    {
         this.log(`Command <${command}> was not recognized`)
-      }
+    }
 
     callback()
   })
